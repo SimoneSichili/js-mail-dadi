5db3499f51c8052fb407a978117e534657600916
Simone <simone.sichili@gmail.com>
2020-11-12 16:50:39 +0100

    Complete "mail" exercise.

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -10,20 +10,24 @@ document.getElementById("mail-utente").innerHTML = mailUtente;
 
 // #2 Crea una lista di email per il confronto
 var listaMail = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com"];
-console.log(listaMail);
+console.log("Lista delle mail per il confronto: " + listaMail);
 
 // #3 Controlla che sia nella lista mail di chi può accedere
+var result = false;
 
 for (var i = 0; i < listaMail.length; i++) {
     
     if (mailUtente == listaMail[i]) {
-        console.log("La tua email è in lista!");
-        document.getElementById("result").innerHTML = "La tua email è in lista!"
-    } else {
-        console.log("Mi dispiace. La tua email non è in lista!");
-        document.getElementById("result").innerHTML = "Mi dispiace. La tua email non è in lista!"
-    }
+        result = true;
+    } 
 
 }
 
 // #4 Stampa un messaggio appropriato sull'esito del controllo
+if (result == true) {
+    console.log("La tua email è in lista!");
+    document.getElementById("result").innerHTML = "La tua email è in lista!"
+} else {
+    console.log("La tua email non è in lista!");
+    document.getElementById("result").innerHTML = "La tua email non è in lista!"
+}
